ad99076ce37ae7f3de72d2ccf19bb145f6a9be22
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-13 07:28:14 +0100

    feat(tofu): Add required tofu version and cloud backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,12 @@
-terraform {}
+terraform {
+  required_version = ">= 1.8.2"
+
+  cloud {
+    organization = "3ware"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "gitops-2024"
+    }
+  }
+}
