ba23d370bcdfc06966439814c3a52e7204f74f78
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-15 10:34:39 +0100

    feat(tofu): Add variable validation for the AWS region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,23 @@
-variable "region" {}
 variable "instance_type" {}
+
+
+locals {
+  valid_regions = ["us-east-1"]
+}
+
+variable "region" {
+  description = "(Required) Name of the AWS region resources will be deployed into."
+  type        = string
+  default     = "us-east-1"
+  nullable    = false
+
+  validation {
+    condition = contains(local.valid_regions, var.region)
+    error_message = format(
+      "Invalid AWS region provided. Received: '%s', Require: '%v'.\n%s",
+      var.region,
+      join(", ", local.valid_regions),
+      "Change the region variable to one that is permitted."
+    )
+  }
+}
