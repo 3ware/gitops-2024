09db3aac083bd412371b3efed908ffcf04e29adb
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-15 06:11:52 +0100

    feat(tofu): Add validation syntax for environment tag

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,13 +7,37 @@ terraform {
   }
 }
 
+locals {
+  # Defines a list of permitted environment tag values. Used by the postcondition in the aws_default_tags data source
+  # to validate the environment tag extrapolated from the workspace name by the local values below
+  valid_environment = ["development", "production"]
+  workspace_split   = split("-", terraform.workspace)
+  environment       = element(local.workspace_split, length(local.workspace_split) - 1)
+}
+
+# trunk-ignore(tflint/terraform_unused_declarations): Data source used to validate environment tag
+data "aws_default_tags" "this" {
+  lifecycle {
+    postcondition {
+      condition = contains(values(self.tags), local.valid_environment)
+      error_message = format(
+        "Invalid environment tag specified. Received: '%s', Require: '%s'.\n%s",
+        local.environment,
+        join(", ", local.valid_environment),
+        "Rename workspace with a valid environment suffix."
+      )
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
   default_tags {
     tags = {
       "3ware:project-id"      = "gitops-2024"
-      "3ware:environment"     = var.environment
+      "3ware:environment"     = local.environment
       "3ware:managed-by-tofu" = true
+      "3ware:workspace"       = terraform.workspace
     }
   }
 }
