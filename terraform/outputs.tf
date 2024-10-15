bbc60459968b9a2b49c5cab2cdbfdf9fa7c737b2
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-15 10:25:54 +0100

    fix(tofu): Add descriptions to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
+output "default_tags" {
+  description = "A map of default tags applied to resources."
+  value       = data.aws_default_tags.this.tags
+}
+
 output "grafana_ip" {
-  value = "http://${aws_instance.grafana_server.public_ip}:3000"
+  description = "The connection details of the grafana server."
+  value       = "http://${aws_instance.grafana_server.public_ip}:3000"
 }
