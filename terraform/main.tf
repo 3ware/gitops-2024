1f97e4e5aa0ec593c83a119f869945daf1499ccf
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-17 11:48:14 +0100

    fix(terraform): Remove from and to port from egress security group
    These values were producing a diff on the plan. The provider
    documentation states that these values should not be defined if
    `ip_protocol = "-1"`
    
    resolves #11

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,9 +78,7 @@ resource "aws_vpc_security_group_egress_rule" "grafana_egress" {
   description       = "Outbound traffic from grafana server"
   security_group_id = aws_security_group.grafana_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
   ip_protocol       = "-1"
-  to_port           = 0
 
   tags = {
     Name = "grafana-egress-sg-rule-${local.environment}"
