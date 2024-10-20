386b67201acfc83435893c6a914f17cf11160966
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-20 15:49:18 +0100

    refactor(terraform): Move files into an environment directory

--- terraform/development/outputs.tf
+++ terraform/development/outputs.tf
@@ -1,8 +1,3 @@
-output "account_id" {
-  description = "The AWS account resources are deployed into."
-  value       = data.aws_caller_identity.current.account_id
-}
-
 output "default_tags" {
   description = "A map of default tags applied to resources."
   value       = data.aws_default_tags.this.tags
