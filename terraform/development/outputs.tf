eed4d99d97557e1ff12b0af8210b19b174ebd3c0
chris3ware <36608309+chris3ware@users.noreply.github.com>
2024-10-20 17:37:38 +0100

    feat(terraform): Add output for AWS account ID

--- terraform/development/outputs.tf
+++ terraform/development/outputs.tf
@@ -1,3 +1,9 @@
+output "aws_account_id" {
+  description = "AWS account number resources are deployed into"
+  value       = data.aws_caller_identity.current.account_id
+  sensitive   = true
+}
+
 output "default_tags" {
   description = "A map of default tags applied to resources."
   value       = data.aws_default_tags.this.tags
